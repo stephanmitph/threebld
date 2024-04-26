e54ddddc9299afbbbafacd9fffe63497c42c1527
Stephan <st.bueeler@hotmail.ch>
2024-04-26 16:03:09 +0200

    ThreeJS setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'threebld';
+export class AppComponent implements OnInit {
+    title = 'threebld';
+
+    ngOnInit() {
+        console.log("AppComponent loaded")
+    }
 }
---
+++ src/app/cube/cube.component.ts
@@ -0,0 +1,22 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { EngineService } from './cube.service';
+
+@Component({
+    selector: 'app-cube',
+    templateUrl: './cube.component.html',
+    styleUrls: ['./cube.component.scss']
+})
+export class CubeComponent implements OnInit {
+
+    @ViewChild('rendererCanvas', { static: true })
+    public rendererCanvas: ElementRef<HTMLCanvasElement>;
+
+    public constructor(private engServ: EngineService) {
+    }
+
+    public ngOnInit(): void {
+        this.engServ.createScene(this.rendererCanvas);
+        this.engServ.animate();
+    }
+
+}
