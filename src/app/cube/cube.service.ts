7742f8a88ff8eb0f64eea409d91c3c02fe431b34
Stephan <st.bueeler@hotmail.ch>
2024-05-01 22:49:25 +0200

    Make cube pretty

--- src/app/cube/cube.service.ts
+++ src/app/cube/cube.service.ts
@@ -1,8 +1,11 @@
 import * as THREE from 'three';
 import { ElementRef, Injectable, NgZone, OnDestroy } from '@angular/core';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { RoundedBoxGeometry } from './RoundedBoxGeometry.js';
+import { RoundedPlaneGeometry } from './RoundedPlaneGeometry.js';
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
+
 
 @Injectable({ providedIn: 'root' })
 export class EngineService implements OnDestroy {
@@ -17,11 +20,12 @@ export class EngineService implements OnDestroy {
 
     // Cube options
     private cubeSize = 3;
-    private sideDepth = 0.1;
-    private padding = 0.2;
-    private spacing = 0.5;
-    private increment = this.cubeSize + this.spacing;
-    private positionOffset = 1; // for 3x3
+    private pieceSize = 3;
+    private piecePadding = 0.1;
+    private pieceSpacing = 0.5;
+    private planePadding = 0.5;
+    private planeDepth = 0.1;
+    private piecePositionOffset = 1; // for 3x3
 
     private pieces: THREE.Object3D[] = [];
 
@@ -56,8 +60,8 @@ export class EngineService implements OnDestroy {
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         // create the scene
         this.scene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 0.1, 50);
-        this.camera.position.set(20, 20, 20);
+        this.camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 10000);
+        this.camera.position.set(40, 40, 40);
         this.camera.lookAt(new THREE.Vector3(0, 0, 0))
         this.scene.add(this.camera);
 
@@ -70,59 +74,92 @@ export class EngineService implements OnDestroy {
         this.scene.add(axesHelper);
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 
-
         this.createCube();
-        this.moveQueue.push({ axis: "U", direction: 1 })
-        this.startMove()
-        setTimeout(() => {
-            this.moveQueue.push({ axis: "D", direction: 1 })
-            this.startMove()
-        }, 2000)
-        setTimeout(() => {
-            this.moveQueue.push({ axis: "B", direction: 1 })
-            this.startMove()
-        }, 4000)
+        setTimeout(() => { this.moveQueue.push(this.tokenToMove("R")); this.startMove(); }, 0)
+        setTimeout(() => { this.moveQueue.push(this.tokenToMove("U")); this.startMove(); }, 1000)
+        setTimeout(() => { this.moveQueue.push(this.tokenToMove("R'")); this.startMove(); }, 2000)
+        setTimeout(() => { this.moveQueue.push(this.tokenToMove("U'")); this.startMove(); }, 3000)
+    }
+    j
+    private tokenToMove(token: string) {
+        return { axis: token.substr(0, 1), direction: token.slice(-1) == "'" ? 1 : -1 }
+    }
+
+    private isCornerPiece(cubeSize: number, x: number, y: number, z: number): boolean {
+        let c = 0;
+        if (x == 0 || x == cubeSize - 1)
+            c++
+        if (y == 0 || y == cubeSize - 1)
+            c++
+        if (z == 0 || z == cubeSize - 1)
+            c++
+        return c >= 3;
+    }
+
+
+    private isMiddlePiece(cubeSize: number, x: number, y: number, z: number): boolean {
+        let c = 0;
+        if (x == 1)
+            c++
+        if (y == 1)
+            c++
+        if (z == 1)
+            c++
+        return c == 2;
+    }
+
+    private isEdgePiece(cubeSize: number, x: number, y: number, z: number): boolean {
+        if (x == 1 && y == 1 && z == 1)
+            return false
+
+        return !this.isCornerPiece(cubeSize, x, y, z);
     }
 
     public createCube(): void {
         for (let x = 0; x < 3; ++x) {
             for (let y = 0; y < 3; ++y) {
                 for (let z = 0; z < 3; ++z) {
+                    if (!this.isEdgePiece(this.cubeSize, x, y, z) && !this.isCornerPiece(this.cubeSize, x, y, z))
+                        continue;
+
                     let piece = new THREE.Group();
 
-                    let colors = [0xFF0000, 0xFFB65C, 0xFFFFFF, 0xFDE410, 0x00FF00, 0x0000FF];
-                    let faceMaterials = colors.map(function(c) {
-                        return new THREE.MeshLambertMaterial({ color: new THREE.Color(c), flatShading: true, transparent: true, opacity: 0.9 });
-                    });
-                    let boxGeometry = new THREE.BoxGeometry(this.cubeSize, this.cubeSize, this.cubeSize);
-                    let boxMaterial = new THREE.MeshMatcapMaterial({ color: new THREE.Color(0xffffff), transparent: true, opacity: 0.5 });
-                    let box = new THREE.Mesh(boxGeometry, faceMaterials);
+                    let boxGeometry = new RoundedBoxGeometry(this.pieceSize, this.pieceSize, this.pieceSize, 3, 0.2);
+                    let boxMaterial = new THREE.MeshMatcapMaterial({ color: new THREE.Color(0x333333), transparent: false, opacity: 0.6 });
+                    let box = new THREE.Mesh(boxGeometry, boxMaterial);
                     piece.add(box);
 
-                    // let sideGeometry = new THREE.BoxGeometry(cubeSize - padding, sideDepth, cubeSize - padding);
-                    // let sideMaterial = new THREE.MeshMatcapMaterial({ color: new THREE.Color(0xFF00FF) });
-                    // let side = new THREE.Mesh(sideGeometry, sideMaterial);
-                    // side.position.set(0, 0, cubeSize / 2 + sideDepth / 2);
-                    // side.rotation.set(Math.PI / 2, 0, 0)
-                    // piece.add(side);
-                    //
-                    // new FontLoader().load("assets/font.json", font => {
-                    //     let fontSize = 1.5;
-                    //     let fontDepth = 0.1;
-                    //     let textGeometry = new TextGeometry("A", {
-                    //         font: font,
-                    //         size: fontSize,
-                    //         depth: fontDepth
-                    //     });
-                    //     textGeometry.center()
-                    //     console.log("FONT LOADED")
-                    //     let text = new THREE.Mesh(textGeometry, new THREE.MeshBasicMaterial({ color: new THREE.Color(0xFF00FF) }));
-                    //     text.position.set(0, 0, cubeSize / 2 + fontDepth / 2 + sideDepth)
-                    //     piece.add(text);
-                    //
-                    // }, () => console.log("FONT LOAD FAILED"))
+                    let planeGeometry = new RoundedPlaneGeometry(this.pieceSize - this.piecePadding - this.planePadding, 0.15, this.planeDepth);
+                    let distance = this.pieceSize / 2 + this.planeDepth / 2;
+                    let planePositions = [];
 
-                    piece.position.set((x - this.positionOffset) * (this.cubeSize + this.padding), (y - this.positionOffset) * (this.cubeSize + this.padding), (z - this.positionOffset) * (this.cubeSize + this.padding));
+                    if (x == 0) planePositions.push(0);
+                    if (x == this.cubeSize - 1) planePositions.push(1);
+                    if (y == 0) planePositions.push(2);
+                    if (y == this.cubeSize - 1) planePositions.push(3);
+                    if (z == 0) planePositions.push(4);
+                    if (z == this.cubeSize - 1) planePositions.push(5);
+
+                    const color = [0xff8c0a, 0xef3923, 0xffef48, 0xfff7ff, 0x41aac8, 0x82ca38]
+                    for (let planePosition of planePositions) {
+                        let planeMaterial = new THREE.MeshMatcapMaterial({ color: new THREE.Color(color[planePosition]) });
+                        let plane = new THREE.Mesh(planeGeometry, planeMaterial);
+
+                        plane.position.set(
+                            distance * [-1, 1, 0, 0, 0, 0][planePosition],
+                            distance * [0, 0, -1, 1, 0, 0][planePosition],
+                            distance * [0, 0, 0, 0, -1, 1][planePosition],
+                        );
+
+                        plane.rotation.set(
+                            Math.PI / 2 * [0, 0, 1, - 1, 0, 0][planePosition],
+                            Math.PI / 2 * [- 1, 1, 0, 0, 2, 0][planePosition],
+                            0
+                        );
+                        piece.add(plane);
+                    }
+
+                    piece.position.set((x - this.piecePositionOffset) * (this.pieceSize + this.piecePadding), (y - this.piecePositionOffset) * (this.pieceSize + this.piecePadding), (z - this.piecePositionOffset) * (this.pieceSize + this.piecePadding));
                     (piece as any)["rubikPosition"] = piece.position.clone();
                     this.pieces.push(piece)
                     this.scene.add(piece)
@@ -139,22 +176,22 @@ export class EngineService implements OnDestroy {
         this.activeGroup = [];
         switch (moveAxis) {
             case "L":
-                this.pieces.forEach(p => { if (this.nearlyEqual((p as any)["rubikPosition"].x, -1 * (this.cubeSize + this.padding))) { this.activeGroup.push(p) } })
+                this.pieces.forEach(p => { if (this.nearlyEqual((p as any)["rubikPosition"].x, -1 * (this.pieceSize + this.piecePadding))) { this.activeGroup.push(p) } })
                 break;
             case "R":
-                this.pieces.forEach(p => { if (this.nearlyEqual((p as any)["rubikPosition"].x, 1 * (this.cubeSize + this.padding))) { this.activeGroup.push(p) } })
+                this.pieces.forEach(p => { if (this.nearlyEqual((p as any)["rubikPosition"].x, 1 * (this.pieceSize + this.piecePadding))) { this.activeGroup.push(p) } })
                 break;
             case "U":
-                this.pieces.forEach(p => { if (this.nearlyEqual((p as any)["rubikPosition"].y, 1 * (this.cubeSize + this.padding))) { this.activeGroup.push(p) } })
+                this.pieces.forEach(p => { if (this.nearlyEqual((p as any)["rubikPosition"].y, 1 * (this.pieceSize + this.piecePadding))) { this.activeGroup.push(p) } })
                 break;
             case "D":
-                this.pieces.forEach(p => { if (this.nearlyEqual((p as any)["rubikPosition"].y, -1 * (this.cubeSize + this.padding))) { this.activeGroup.push(p) } })
+                this.pieces.forEach(p => { if (this.nearlyEqual((p as any)["rubikPosition"].y, -1 * (this.pieceSize + this.piecePadding))) { this.activeGroup.push(p) } })
                 break;
             case "F":
-                this.pieces.forEach(p => { if (this.nearlyEqual((p as any)["rubikPosition"].z, 1 * (this.cubeSize + this.padding))) { this.activeGroup.push(p) } })
+                this.pieces.forEach(p => { if (this.nearlyEqual((p as any)["rubikPosition"].z, 1 * (this.pieceSize + this.piecePadding))) { this.activeGroup.push(p) } })
                 break;
             case "B":
-                this.pieces.forEach(p => { if (this.nearlyEqual((p as any)["rubikPosition"].z, -1 * (this.cubeSize + this.padding))) { this.activeGroup.push(p) } })
+                this.pieces.forEach(p => { if (this.nearlyEqual((p as any)["rubikPosition"].z, -1 * (this.pieceSize + this.piecePadding))) { this.activeGroup.push(p) } })
                 break;
             default:
                 console.log("Illegal move axis")
